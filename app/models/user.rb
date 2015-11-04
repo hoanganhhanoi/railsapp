b9a57b80097b27dfb68dbad6f42b606b96af0837
Nguyen Hoang Anh <nguyenhoanganhbachkhoahanoi@outlook.com.vn>
2015-11-05 00:07:15 +0700

    add index to element email and validate element

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ActiveRecord::Base
+
+  # Validates
+  validates :name, presence: true, length: { maximum: 50 }
+
+  # Expression email
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: true
+
+  # fix element before save
+  before_save {
+    self.email = email.downcase
+  }
+  
 end
