51774daabe62d394c9f3d20a43aa6c333bde703e
Nguyen Hoang Anh <nguyenhoanganhbachkhoahanoi@outlook.com.vn>
2015-11-05 05:25:25 +0700

    complete delete user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
 
   # Check log in before access to edit, update
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
 
   def index
@@ -47,6 +48,19 @@ class UsersController < ApplicationController
     end
   end
 
+  # Destroy user
+  def destroy
+    
+    if User.find(params[:id]).destroy
+      flash[:success] = "Xoa thanh vien thanh cong"
+    else
+      flash[:danger]  = "Co van de trong phien giao dich"
+    end
+
+    redirect_to users_url
+
+  end
+
   private
 
     # Get params from views 
@@ -68,6 +82,10 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to root_url unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to root_url unless current_user.admin?
     end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,8 @@
 User.create!(name: "Anh",
              email: "phangnhaudiem@gmail.com",
              password: "123456",
-             password_confirmation: "123456")
+             password_confirmation: "123456",
+             admin: true)
 
 99.times do |n|
   name = Faker::Name.name
