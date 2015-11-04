cbcf8f6b558582a458f34136ac5977c87eb62d70
Nguyen Hoang Anh <nguyenhoanganhbachkhoahanoi@outlook.com.vn>
2015-11-04 23:40:19 +0700

    create screen new signup

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,11 @@ Rails.application.routes.draw do
   # link to about page
   get 'about' => 'static_pages#about'
 
-  #link to contact page
+  # link to contact page
   get 'contact' => 'static_pages#contact'
+
+  # link to signup 
+  get 'signup' => 'users#new'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
