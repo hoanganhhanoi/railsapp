507085c538d3cd1f33ed095182cd3bb91c1da71a
Nguyen Hoang Anh <nguyenhoanganhbachkhoahanoi@outlook.com.vn>
2015-11-05 02:03:51 +0700

    create session user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
+
+  # Set new Session
   def new
   end
+
+  # Create new Session
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+    else
+      flash[:danger] = "Email hoac mat khau khong dung"
+      render 'new'
+    end
+  end
+
+  # Destroy session
+  def destroy
+  end
+
 end
