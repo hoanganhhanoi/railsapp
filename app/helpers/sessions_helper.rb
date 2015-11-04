e995d032a9079b6d7353ad89377fe61f4a8f230e
Nguyen Hoang Anh <nguyenhoanganhbachkhoahanoi@outlook.com.vn>
2015-11-05 03:42:54 +0700

    complete login logout last

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,22 +3,46 @@ module SessionsHelper
   # User login
   def log_in(user)
     session[:user_id] = user.id
+    p :user_id
   end
 
   # user logout
   def log_out
+    forget_user(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
 
   # Return current user logged
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if(user_id = session[:user_id])
+      @current_user ||= User.find_by(id: session[:user_id])
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   # Return true if user logged in
   def logged_in?
     !current_user.nil?
+  end
+
+  # Remember a user
+  def remember_user(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # Forget user when user log out
+  def forget_user(user)
+    user.forget_user
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  # avaiable remember_token
+  attr_accessor :remember_token
 
   # Validates
   validates :name, presence: true, length: { maximum: 50 }
@@ -17,5 +19,39 @@ class User < ActiveRecord::Base
   # secure password
   has_secure_password
   validates :password, length: { minimum: 6 }
+
+  # List function of ModelUser
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Return hash digest 
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    return BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Return a random token
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Return true if token match digest
+  def authenticated?(remember_token)
+    # if remember_digest is nil return false
+    if remember_digest.nil?
+      return false
+    else
+      BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+  end
+
+  # Forget user when user log out
+  def forget_user
+    update_attribute(:remember_digest, nil)
+  end
 
 end
